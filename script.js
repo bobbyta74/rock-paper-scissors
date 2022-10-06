abe5da2b249cd23a7f91a80be20a0e0f27e12386
Bobby Ta <bobbyta74@gmail.com>
2022-10-06 08:37:35 +0200

    Complete console-based version of the game

--- script.js
+++ script.js
@@ -11,5 +11,26 @@ function getPlayerChoice () {
     let userinput = "nothing";
     while (userinput != "rock" && userinput != "paper" && userinput != "scissors") {
         userinput = prompt("Enter rock, paper or scissors");
+        userinput.toLowerCase();
+    }
+    return userinput;
+}
+
+function playRound(playerSelection, computerSelection) {
+    const playerwinsr = playerSelection == "rock" && computerSelection == "scissors";
+    const playerwinsp = playerSelection == "paper" && computerSelection == "rock";
+    const playerwinss = playerSelection == "scissors" && computerSelection == "paper";
+    if (playerwinsr || playerwinsp || playerwinss) {
+        return "You win! \n" + playerSelection + " beats " + computerSelection; 
+    } else {
+        return "You lose! \n" + computerSelection + " beats " + playerSelection; 
     }
 }
+
+function game() {
+    for (let i=0;i<5;i++) {
+        console.log(playRound(getPlayerChoice(), getComputerChoice()));
+    }
+}
+
+game();
